86128c76ff72f996aaea085666dc3080b77b256c
aerinon <randall.rupper@sirsidynix.com>
2022-03-08 08:47:25 -0700

    Add major_only algorithm to code

--- BaseClasses.py
+++ BaseClasses.py
@@ -2727,8 +2727,12 @@ e_dmg = {"default": 0, "shuffled": 1, "random": 2}
 
 # byte 8: RRAA A??? (restrict boss mode, algorithm, ? = unused)
 rb_mode = {"none": 0, "mapcompass": 1, "dungeon": 2}
-# algorithm: todo with "biased shuffles"
-algo_mode = {"balanced": 0, "equitable": 1, "vanilla_fill": 2, "dungeon_only": 3, "district": 4}
+# algorithm:
+algo_mode = {"balanced": 0, "equitable": 1, "vanilla_fill": 2, "dungeon_only": 3, "district": 4, 'major_only': 5}
+boss_mode = {"none": 0, "simple": 1, "full": 2, "chaos": 3, 'random': 3, 'unique': 4}
+
+
+# byte 10: settings_version
 
 # additions
 # psuedoboots does not effect code
